9dc1ee8e2139062804883a78fb7c4aa582703d61
Paulína Palíková <paulipalik@gmail.com>
2021-05-01 12:35:43 +0200

    Uprava btn objednania

--- src/index.js
+++ src/index.js
@@ -6,16 +6,36 @@ console.log('funguju!');
 // V JavaScriptu vyberte ze stránky ikonku #nav-btn. Připojte k ní posluchač události click. Tento posluchač prvku nav odebere nebo přidá třídu nav-closed. Klikáním na ikonku tak můžeme zobrazovat nebo skrývat navigaci.
 const navBar = document.querySelector('nav');
 const navElm = document.querySelector('#nav-btn');
-console.log(navElm);
+const navLinkElm = document.querySelectorAll('nav a');
+
 navElm.addEventListener('click', () => {
   navBar.classList.toggle('nav-closed');
 });
 
 // Navigaci budeme chtít schovat i po přesunutí na nějakou sekci. Připojte tedy posluchač události také na všechny položky navigace. Zařiďte, aby se navigace při kliknutí na libovolnou její položku schovala. Zde se vám jistě bude hodit metoda querySelectorAll.
 
-const hides = document.querySelectorAll(".hide");
-for (i = 0, i < hides.length; i += 1) {
-  hides[i].addEventListener("click", () => {
-    navBar.classList.toggle("nav-closed"); 
-  }); 
+for (let i = 0; i < navLinkElm.length; i++) {
+  navLinkElm[i].addEventListener('click', () => {
+    navBar.classList.add('nav-closed');
+  });
 }
+
+// Na tlačíko se třídou order-btn pověste posluchač, při kliknutí změní nápis na tlačítku na Zrušit. Zároveň přidá na prvek se třídou drink__cup třídu drink__cup--selected.
+
+// Při opětovném kliknutí na tlačítko chceme zařídit, aby se objednávka zrušila a změny na prvku .drink__cup i na tlačítku .order-btn se vrátily (odebrala se třída drink__cup--selected a vrátil se text tlačítka na Objednat). Tohoto můžete docílit například tak, že si vytvoříte globální proměnnou ordered, která bude obsahovat true nebo false podle toho, zde je nápoj objednaný či nikoliv.
+
+const orderBtnElm = document.querySelector('.order-btn');
+const drinkCupElm = document.querySelector('.drink__cup');
+let notOrdered = true;
+
+orderBtnElm.addEventListener('click', () => {
+  if (notOrdered) {
+    orderBtnElm.textContent = 'Zrušit';
+    drinkCupElm.classList.add('drink__cup--selected');
+    notOrdered = false;
+  } else {
+    orderBtnElm.textContent = 'Objednat';
+    drinkCupElm.classList.remove('drink__cup--selected');
+    notOrdered = true;
+  }
+});
